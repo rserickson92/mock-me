78dbf24db9e80c8cb86161ec39059df2ba11f0c9
rserickson92 <rserickson92@gmail.com>
2014-11-22 04:01:02 -0500

    implement interview request ('post') creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,31 @@
 class PostsController < ApplicationController
+	before_action :authorize
   def index
   	@post = Post.new
   	@reqs = Post.get_all
   end
 
   def create
+  	params = posts_params
+  	post = Post.new(body: params[:body], user: current_user)
+  	if post.save
+	  	render partial: "post", locals: { locals: {
+	  		username: current_user.name,
+	  		uid: current_user.id,
+	  		body: params[:body]
+	  	} }
+	  else
+	  	render "index"
+	  end
   end
 
   def destroy
   end
+
+  private
+
+  def posts_params
+  	params.require(:post)
+  end
+
 end
