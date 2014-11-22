c1ab9613d81c85b89b9660f4babbd7bb5b658909
rserickson92 <rserickson92@gmail.com>
2014-11-22 11:40:55 -0500

    move random interview question generation to interviews

--- app/helpers/interview_helper.rb
+++ app/helpers/interview_helper.rb
@@ -1,2 +1,10 @@
 module InterviewHelper
+	def get_random_question
+		page_range = 30
+		page_num = rand(page_range)+1
+		doc = Nokogiri::HTML(open("http://www.careercup.com/page?n=#{page_num}"))
+		questions = doc.css("li.question .entry p")
+		question_num = rand(questions.length)+1
+		questions[question_num].text
+	end
 end
